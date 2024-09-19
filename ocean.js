e2c3dcb52c78057a66666596a343b1df9f02d286
boravellicharan7 <boravellicharan6@gmail.com>
2024-09-19 19:15:45 +0530

    added

---
+++ ocean.js
@@ -0,0 +1,38 @@
+var a = 10;
+console.log(a);
+
+let b = 20;
+console.log(b);
+
+const c = 30;
+console.log(c);
+
+{
+    var aa = "ravipati";
+    console.log(aa);
+    {
+        let d="gayu...";
+        console.log(d);
+
+        const dd=["charan","surya",10]
+        console.log(dd)
+
+        var e={
+            name:"keerthi",
+            age:21
+        }
+        console.log(e);
+        
+    }
+}
+
+
+{
+    let bb = "surya";
+    console.log(bb);
+}
+
+{
+    const cc = "nani";
+    console.log(cc);
+}
